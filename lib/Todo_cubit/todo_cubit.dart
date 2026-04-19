d25f48e93c273ae74a2b9e671eacc7274139bc69
ziad-mahdy <zyadmahdy01@gmail.com>
2026-04-19 12:55:04 +0200

    Todo App

--- lib/Todo_cubit/todo_cubit.dart
+++ lib/Todo_cubit/todo_cubit.dart
@@ -10,6 +10,14 @@ class TodoCubit extends Cubit<List<String>> {
   void removeTask(int index) {
     final newList = [...state];
     newList.removeAt(index);
-    emit(newList);
+    emit(newList);  
   }
+
+  void editTask(int index, String newTask) {
+  final newList = [...state];
+  
+  newList[index] = newTask;
+  
+  emit(newList);
+}
 }
--- lib/screens/Todo_screen.dart
+++ lib/screens/Todo_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/Todo_cubit/todo_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../Todo_cubit/todo_cubit.dart';
 
+class TodoScreen extends StatefulWidget {
+  const TodoScreen({super.key});
 
-class TodoScreen extends StatelessWidget {
+  @override
+  State<TodoScreen> createState() => _TodoScreenState();
+}
+
+class _TodoScreenState extends State<TodoScreen> {
   final TextEditingController _controller = TextEditingController();
-
-  TodoScreen({super.key});
+  final TextEditingController _editController = TextEditingController();
+  int _editingIndex = -1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Todo List ")),
+      appBar: AppBar(title: const Text("Todo List")),
       body: Column(
         children: [
           Padding(
@@ -29,7 +35,7 @@ class TodoScreen extends StatelessWidget {
                   onPressed: () {
                     if (_controller.text.isNotEmpty) {
                       context.read<TodoCubit>().addTask(_controller.text);
-                      _controller.clear(); 
+                      _controller.clear();
                     }
                   },
                 ),
@@ -42,11 +48,51 @@ class TodoScreen extends StatelessWidget {
                 return ListView.builder(
                   itemCount: tasks.length,
                   itemBuilder: (context, index) {
+                    bool isEditing = _editingIndex == index;
                     return ListTile(
-                      title: Text(tasks[index]),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete, color: Colors.red),
-                        onPressed: () => context.read<TodoCubit>().removeTask(index),
+                      title: isEditing
+                          ? TextField(
+                              controller: _editController,
+                              autofocus: true,
+                            )
+                          : Text(tasks[index]),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (isEditing) ...[
+                            IconButton(
+                              icon: const Icon(Icons.check, color: Colors.green),
+                              onPressed: () {
+                                context.read<TodoCubit>().editTask(index, _editController.text);
+                                setState(() {
+                                  _editingIndex = -1;
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.close, color: Colors.red),
+                              onPressed: () {
+                                setState(() {
+                                  _editingIndex = -1;
+                                });
+                              },
+                            ),
+                          ] else ...[
+                            IconButton(
+                              icon: const Icon(Icons.edit, color: Colors.blue),
+                              onPressed: () {
+                                setState(() {
+                                  _editingIndex = index;
+                                  _editController.text = tasks[index];
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () => context.read<TodoCubit>().removeTask(index),
+                            ),
+                          ],
+                        ],
                       ),
                     );
                   },
